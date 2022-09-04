548d5ccfa3ba1f685be73522d0efd6bdd2ac58c4
Stanley Okonkwo <okonkwostanley67@yahoo.com>
2022-09-04 15:12:53 -0700

    Update Question 3 - Pig Latin.cs

--- Question 3 - Pig Latin.cs
+++ Question 3 - Pig Latin.cs
@@ -1,7 +1,7 @@
 
 
 using System;
-
+using System.Collections.Generic;
 public class HelloWorld
 { 
         public static void pig_latin(string words) 
@@ -13,10 +13,26 @@ public class HelloWorld
                 Console.WriteLine(rt + et +"ay");
             }
         }
+         public static void latin_pig(string words) 
+        {
+            
+            string[] word = words.Split(" ");
+            foreach(string st in word){
+                string remove_ay = st.Remove(st.Length -2 );
+                char last_char = remove_ay[remove_ay.Length-1];
+                string final_word = st.Remove(st.Length -3 );
+                
+                Console.WriteLine($"{last_char}{final_word}");
+            }
+        }
 
     public static void Main(string[] args)
-    {
-         pig_latin("I just got executed!");
+    {   
+        pig_latin("The quick brown fox");
+        Console.WriteLine("\n");
+        latin_pig("heTay uickqay rownbay oxfay");
+        
+
      
     }
 }
