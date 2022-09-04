9102b39f81196e1afc8c89c1f0f060fef4a92d37
Stanley Okonkwo <okonkwostanley67@yahoo.com>
2022-09-04 10:41:35 -0700

    commits

--- Question 2 - Guess game.cs
+++ Question 2 - Guess game.cs
@@ -1,38 +1,48 @@
-
 using System;
-using System.Collections.Generic;
-
-public class HelloWorld
+using System.Collections.Generic;					
+public class Program
 {
-    public static void Main(string[] args)
-    {   
-        int user_tries = 0;
-        while (user_tries <= 5)
-        {
-            user_tries +=1;
-            
-        Console.WriteLine("Guess secret number between range 1-5");
-        int enter_number = int.Parse(Console.ReadLine());
-        Random rd = new Random();
-        int rand_num = rd.Next(5);
-       if (enter_number < rand_num){
-            Console.WriteLine("Mininum than target");
-        }
-        
-        else if ( enter_number > rand_num ){
-            Console.WriteLine("Maxmimum than target");
-        }
-       else{
-            Console.WriteLine($"that's correct Secret number is:{rand_num}");
-            break;
-       }
-       
-        Console.WriteLine($"random number is {rand_num}");
-                Console.WriteLine($"Try {user_tries}"); 
-        }
-        
-        
-        Console.WriteLine($"Total tries {user_tries}");
-        
-    }
+	public static void Main()
+	{
+		int user_tries = 0;
+        var user_inputs = new List<int>();
+
+		 while (user_tries <= 5)
+            {
+               
+                Console.WriteLine("guess from 1-5 numbers ");
+                int enter_number = int.Parse(Console.ReadLine());
+                   
+                if (user_inputs.Contains(enter_number))
+                {
+                     user_tries -= 1;
+                    Console.WriteLine($"that's a one try {user_tries}");
+                    continue;
+                    
+                }
+
+                user_inputs.Add(enter_number);
+                Random rd = new Random();
+                int rand_num = rd.Next(5);
+               user_tries += 1;
+                if (enter_number < rand_num)
+                {
+                    Console.WriteLine($"Mininum");
+                }
+
+                else if (enter_number > rand_num)
+                {
+                    Console.WriteLine("Maxmimum ");
+                }
+                else
+                {
+                    Console.WriteLine($"that's correct {rand_num}");
+                    break;
+                }
+
+                Console.WriteLine($"random number is {rand_num}");
+                Console.WriteLine($" tries count is {user_tries}");
+            }
+            Console.WriteLine($"Total tries is {user_tries}");
+	}
 }
